be9aebc99bc30c7a8af352bd5a0ff3c107447e9e
Mayowa Ajiboye <mayaajiboye@gmail.com>
2023-02-27 16:56:03 -0500

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,15 @@
-#!/usr/bin/env python3
-
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+fido = Dog("Fido")
+print(fido.breed)
+
+snoopy = Dog("Snoppy", "Terrier")
+print(snoopy.name)
+# Any
+
+# snoopy = Dog("Snoopy", "Tennis Ball")
+# print(snoopy.favorite_toy)
+# # Tennis Ball
